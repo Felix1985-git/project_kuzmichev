5a0baee4bde9300d6358161902e9a1562ae3c25e
FELIX <kuzmichoualiaksei@gmail.com>
2021-03-03 22:09:51 +0300

    update db select

--- src/academy/belhard/dbReader/FullDataReader.java
+++ src/academy/belhard/dbReader/FullDataReader.java
@@ -11,11 +11,11 @@ import java.util.List;
 public class FullDataReader {
 
     private static final String SELECT_FULL_DATA = "SELECT fl.number, fl.date, fl.time, pl.tale_number, " +
-            "CONCAT_WS (' ', pl.brand, pl.model), pl.capacity, CONCAT (CONCAT_WS (' ', p.last_name, " +
-            "substr(p.first_name,1,1)),+'.'), \n" +
-            "CONCAT (p.code, +' (',p.rang,+')')\n" +
+            "CONCAT_WS (' ', pl.brand, pl.model) AS brand_model, pl.capacity, \n" +
+            "CONCAT (CONCAT_WS (' ', p.last_name, substr(p.first_name,1,1)),+'.') AS full_name, \n" +
+            "CONCAT (p.code, +' (',p.rang,+')') AS code_rang\n" +
             "FROM flights fl\n" +
-            "JOIN planes pl ON fl.planes_id = pl.id \n" +
+            "JOIN planes pl ON fl.planes_id = pl.id\n" +
             "JOIN pilots p ON fl.pilots_id = p.id;";
 
     public static List<FullData> read() {
@@ -31,10 +31,10 @@ public class FullDataReader {
                 Date flightDate = resultSet.getDate("date");
                 Time flightTime = resultSet.getTime("time");
                 String planeTaleNumber = resultSet.getString("tale_number");
-                String planeBrandModel = resultSet.getString("CONCAT_WS (' ', pl.brand, pl.model)");
+                String planeBrandModel = resultSet.getString("brand_model");
                 Integer planeCapacity = resultSet.getInt("capacity");
-                String pilotFullName = resultSet.getString("CONCAT (CONCAT_WS (' ', p.last_name, substr(p.first_name,1,1)),+'.')");
-                String pilotCodeRang = resultSet.getString("CONCAT (p.code, +' (',p.rang,+')')");
+                String pilotFullName = resultSet.getString("full_name");
+                String pilotCodeRang = resultSet.getString("code_rang");
 
                 result.add(new FullData(flightNumber, flightDate, flightTime, planeTaleNumber, planeBrandModel, planeCapacity, pilotFullName, pilotCodeRang));
             }
